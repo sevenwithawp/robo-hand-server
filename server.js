6cda2a3704147efcace49ab8a022750039471160
Ubuntu <ubuntu@ip-172-31-29-15.ec2.internal>
2018-06-02 19:37:51 +0000

    Added unrefactored move endpoint functionality

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+var exec = require('child_process').execSync, child;
 
 console.log('app: ', app)
 
@@ -14,6 +15,18 @@ app.get('/hello', function(req, res) {
   res.send(`hello ${name}`);
 });
 
+app.get('/move', function(req, res) {
+  const boardState = req.query.board;
+  const response = exec(`java -jar com.checkers.jar ${boardState}`).toString();
+  console.log('full response: ', response);
+  console.log('Response from app: ', response.slice(response.length - 5, response.length));
+  const newMovePosition = response.slice(response.length - 5, response.length);
+
+  const lettersMap = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
+  const jsonResponse = { from: `${lettersMap[parseInt(newMovePosition[0])+1]}${9-parseInt(newMovePosition[1])}`, to: `${lettersMap[parseInt(newMovePosition[2])+1]}${9-parseInt(newMovePosition[3])}`};
+  console.log('jsonResponse: ', jsonResponse);
+  res.json(jsonResponse);
+});
 
 // Set the listen port
 const port = process.env.PORT || 8000;
